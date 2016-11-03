dadf8e302d20701535b508d1dba3c3987514a00d
Dave Compton <davecompton7@gmail.com>
2016-11-03 23:51:22 +0000

    changed names ... other minor changes

--- Main.hs
+++ Main.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Reflex
 import Reflex.Dom
-import Data.Text as DT
+import Data.Text (Text, pack,unpack) 
 import Data.Maybe
-import Data.Map
+import Data.Map (Map, fromList)
 import Text.Read
 
 data Ellipse = Ellipse { a :: Float
@@ -37,32 +37,43 @@ toMap :: Maybe Ellipse -> Map Int Ellipse
 toMap  = 
     fromList
     .fmap (\e -> ((,) 0 (fromMaybe (Ellipse 0.0 0.0 0.0) e))) 
-    .Prelude.filter isJust -- Get rid of Nothing 
+    .filter isJust -- Get rid of Nothing 
     .(:[]) -- Put the Maybe into a (one item) list 
 
-circleAttrs :: Ellipse -> Map Text Text
-circleAttrs (Ellipse a b n) =
-    fromList [ ( "cx",    "0")
-             , ( "cy",    "0")
-             , ( "r",     pack $ show a)
-             , ( "style", "fill:green")
-             ] 
+lineAttrs :: (Float,Float) -> Ellipse -> Map Text Text
+lineAttrs (p0, p1) e =
+    let
+        evalPoint p (Ellipse a b n) = 
+            let x = p*a
+                y = ((1 - (x/a) ** n) ** (1/n) ) * b
+            in (x,y)
+    
+        (x1,y1) = evalPoint p0 e
+        (x2,y2) = evalPoint p1 e
+    in fromList [ ( "x1",    pack $ show (x1*200.0))
+                , ( "y1",    pack $ show (y1*200.0))
+                , ( "x2",    pack $ show (x2*200.0))
+                , ( "y2",    pack $ show (y2*200.0))
+                , ( "style", "stroke:red;stroke-width:2")
+                ]    
 
 showCircle :: MonadWidget t m => Int -> Dynamic t Ellipse -> m ()
 showCircle _ dEllipse = do
+    let ps = take 201 $ iterate (+0.005) 0.0
+        ps2 = zip ps $ tail ps
     elStopPropagationNS svgns "g" Mousemove $ 
-        elDynAttrNS' svgns "circle" (circleAttrs <$> dEllipse) $ return ()
-    return ()
+        mapM_ (\p -> (elDynAttrNS' svgns "line" (lineAttrs p <$> dEllipse) $ return ())) ps2
 
 view :: MonadWidget t m => m ()
 view = do 
-    inputA <- el "div" $ textInput def
-    inputB <- el "div" $ textInput def
-    inputN <- el "div" $ textInput def
-    let dMaybeAB = zipDynWith toEllipse (value inputA) (value inputB) 
-        dMaybeABN = zipDynWith ($) dMaybeAB (value inputN)
+    ta <- (el "div" $ textInput def)
+    tb <- (el "div" $ textInput def)
+    tn <- (el "div" $ textInput def)
+    let 
+        ab = zipDynWith toEllipse (value ta) (value tb)
+        abn = zipDynWith ($) ab (value tn)
 
-        dMap = fmap toMap dMaybeABN
+        dMap = fmap toMap abn
         
         dAttrs = constDyn $ 
                         fromList [ ("width" , "600")
@@ -70,7 +81,7 @@ view = do
                                  , ("style" , "border:solid; margin:8em")
                                  ]
 
-    dynText $ fmap (pack.showError) dMaybeABN
+    dynText $ fmap (pack.showError) abn
     elDynAttrNS' svgns "svg" dAttrs $ listWithKey dMap showCircle
 
     return ()
